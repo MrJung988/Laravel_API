d1891180df8dc7374e534a5667edfe349f2ae6cf
Samir Karki <karkisamir99@gmail.com>
2022-12-27 11:37:00 +0545

    Create REST API Routes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+Route::middleware('auth:api')->group(function () {
+    Route::resource('blog', BlogController::class);
+});
